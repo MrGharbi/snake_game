01737a1435c06a591975aefcdcfe2e9ed6b6ba79
Mohamed Amine Gharbi <mag@Mohameds-MacBook-Pro.local>
2020-12-13 17:58:51 +0100

    First version of Snake Game

---
+++ food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("blue")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        randomx = random.randint(-250, 250)
+        randomy = random.randint(-250, 250)
+        self.goto(randomx, randomy)
